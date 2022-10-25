92d27a389ce8d541395eb8532e10780f87e0cc61
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-10-25 14:12:06 +0300

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,47 @@
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let newName = '';
+
+const getContent = () => {
+  rl.question('Write your content: ', (content) => {
+    if (content.length === 0) {
+      // eslint-disable-next-line no-console
+      console.log('Content most be more then 0 character');
+      getContent();
+    } else {
+      fs.writeFileSync(newName, content, (err, data) => {
+        if (err) {
+          // eslint-disable-next-line no-console
+          console.log('Error');
+        } else {
+          // eslint-disable-next-line no-console
+          console.log('Success');
+        }
+      });
+      rl.close();
+    }
+  });
+};
+
+const terminal = () => {
+  rl.question('Write your file name: ', (fileName) => {
+    if (fileName.length === 0) {
+      // eslint-disable-next-line no-console
+      console.log('Name most be more then 0 character!');
+      terminal();
+    } else {
+      newName = fileName;
+      getContent();
+    }
+  });
+};
+
+terminal();
